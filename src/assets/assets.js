26f375f8356ad126b32cb4d94c219a99beded6dc
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-06-19 02:42:26 -0300

    Update `Assets`

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,9 +1,6 @@
 import basket_icon from './basket_icon.png'
 import cart_icon from './cart_icon.svg'
-import logo from './logo.png'
 import logoTd from './mercado_td.png'
-import header_img from './header_img.png'
-import search_icon from './search_icon.png'
 import menu_open from './menu_open.svg'
 import menu_close from './menu_close.svg'
 
@@ -19,18 +16,11 @@ import cat_8 from './cat_8.png'
 import add_icon_white from './add_icon_white.png'
 import add_icon_green from './add_icon_green.png'
 import remove_icon_red from './remove_icon_red.png'
-import app_store from './app_store.png'
-import play_store from './play_store.png'
 import linkedin_icon from './linkedin_icon.png'
 import facebook_icon from './facebook_icon.png'
 import twitter_icon from './twitter_icon.png'
 import cross_icon from './cross_icon.png'
-import selector_icon from './selector_icon.png'
-import rating_starts from './rating_starts.png'
-import profile_icon from './profile_icon.png'
 import bag_icon from './bag_icon.png'
-import logout_icon from './logout_icon.png'
-import parcel_icon from './parcel_icon.png'
 import amex from './amex.svg'
 import boleto from './boleto.svg'
 import caixa from './caixa.svg'
@@ -44,26 +34,16 @@ import close from './close.svg'
 import qrcode from './qrcode-duan.png'
 
 export const assets = {
-    logo,
     basket_icon,
     cart_icon,
-    header_img,
-    search_icon,
-    rating_starts,
     add_icon_green,
     add_icon_white,
     remove_icon_red,
-    app_store,
-    play_store,
     linkedin_icon,
     facebook_icon,
     twitter_icon,
     cross_icon,
-    selector_icon,
-    profile_icon,
-    logout_icon,
     bag_icon,
-    parcel_icon,
     logoTd,
     amex,
     boleto,
