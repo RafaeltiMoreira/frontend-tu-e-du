fc4cbe0c09778d312a00357f527732e2770220d9
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-07-02 23:25:51 -0300

    Inclusão de produto

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -92,7 +92,8 @@ export const menu_list = [
     {
         menu_name: "Vale presente",
         menu_image: cat_8
-    }]
+    }
+]
 
 export const gift_list = [
     {
@@ -110,70 +111,80 @@ export const gift_list = [
         price: 220.00,
         description: "Jogo de lençol King 4 peças 400 fios 100% algodão, super macio estampado (floral roxo).",
         category: "Enxoval"
-    }, {
+    },
+    {
         _id: "3",
         name: "Jogo de lençol capuccino",
         image: "https://utfs.io/f/00ec4d4d-1b51-4156-a8aa-54b13a8d3ca8-xyw41w.png",
         price: 239.99,
         description: "Jogo de cama lençol 400 fios 3 peças ponto palito casa Dona (Queen, capuccino).",
         category: "Enxoval"
-    }, {
+    },
+    {
         _id: "4",
         name: "Jogo de cama estampado",
         image: "https://utfs.io/f/cff60d73-1a1e-4c5a-ad75-c8d9f370b434-xz1dwp.png",
         price: 280.89,
         description: "Jogo de cama King Size 100% algodão estampado 4pçs, Romanza Vivaldi Premium.",
         category: "Enxoval"
-    }, {
+    },
+    {
         _id: "5",
         name: "Geladeira Brastemp",
         image: "https://utfs.io/f/3b1d9882-f866-45d0-b23e-037f28bc53b0-qprdmk.png",
         price: 2821.73,
         description: "Geladeira Brastemp Frost Free Duplex 386 litros com Altura Flex cor Inox.",
         category: "Eletrodomésticos"
-    }, {
+    },
+    {
         _id: "6",
         name: "Fritadeira Elétrica",
         image: "https://utfs.io/f/c191adf7-67db-4c8e-a987-462c1d809bac-s4jp3n.png",
         price: 351.82,
         description: "Fritadeira Elétrica sem Óleo/Air Fryer Arno, moderna vermelha 3,5L com timer.",
         category: "Eletrodomésticos"
-    }, {
+    },
+    {
         _id: "7",
         name: "Robô Aspirador de Pó",
         image: "https://utfs.io/f/4c3a5e0d-7807-4755-8e76-b197ec23e652-twvrrw.png",
         price: 699.90,
         description: "Robô Aspirador de Pó KaBuM! Smart 100, Preto, recarregamento automático.",
         category: "Eletrodomésticos"
-    }, {
+    },
+    {
         _id: "8",
         name: "Purificador de água",
         image: "https://utfs.io/f/d132a848-bac1-4cee-8a2b-fdb345def177-fj8wqy.png",
         price: 1474.40,
         description: "Purificador de água vermelho Soft Slim, Everest.",
         category: "Eletrodomésticos"
-    }, {
+    },
+    {
         _id: "9",
         name: "Maleta de ferramentas",
         image: "https://utfs.io/f/28d43724-2c90-4be4-8a73-35643fd704a3-hvw346.png",
         price: 156.90,
         description: "Kit de Ferramentas 129 Peças com Maleta, Sparta.",
         category: "Ferramentas"
-    }, {
+    },
+    {
         _id: "10",
         name: "Parafusadeira e Furaderia",
         image: "https://utfs.io/f/9afe14f6-17e6-4906-8108-801ed28fc6e9-82vw9m.png",
         price: 1079.00,
         description: "Parafusadeira e Furaderia de 13mm Brushless Motor a Bateria 20V, com 2 Baterias.",
         category: "Ferramentas"
-    }, {
+    },
+    {
         _id: "11",
         name: "Lavadora de Alta Pressão",
         image: "https://utfs.io/f/79042d09-020b-4a7b-9234-f51595a45c3b-8ln035.png",
         price: 597.55,
         description: "Lavadora de Alta Pressão Electrolux 2200 PSI, Bico Turbo e Engate rápido.",
         category: "Ferramentas"
-    }, {
+    },
+    {
         _id: "12",
         name: "Jogo de chaves",
         image: "https://utfs.io/f/1655916a-69a7-4564-8d36-16a82ea1f20f-miofi0.png",
@@ -196,70 +207,80 @@ export const gift_list = [
         price: 223.85,
         description: "Jogo de toalhas Buddemeyer Concórdia, gigante vermelho 4 peças.",
         category: "Banho"
-    }, {
+    },
+    {
         _id: "15",
         name: "Roupão de Banho Rosê",
         image: "https://utfs.io/f/42699983-a928-4341-89cf-cdfaec0d6d1c-yqel.png",
-        price: 229.90,
+        price: 120.00,
         description: "Roupão de Banho Rosê Kacyumara Vintage Microfibra (M).",
         category: "Banho"
-    }, {
+    },
+    {
         _id: "16",
         name: "Roupão de Banho Chumbo",
         image: "https://utfs.io/f/d2f71595-65fa-47c8-9f62-7668e3466ae8-yqes.png",
-        price: 229.90,
+        price: 120.00,
         description: "Roupão de Banho Chumbo Andreza Microfibra Tress (G) Manga Longa.",
         category: "Banho"
-    }, {
+    },
+    {
         _id: "17",
         name: "Smart TV 43",
         image: "https://utfs.io/f/e85d4172-24fd-40e8-a2b3-f66f5de1e04c-2v6.png",
         price: 1899.05,
         description: "Smart TV 43” 4K Ultra HD LED LG, Wi-Fi Bluetooth Alexa 3 HDMI.",
         category: "Casa&Cozinha"
-    }, {
+    },
+    {
         _id: "18",
         name: "Frigobar Retrô",
         image: "https://utfs.io/f/9b701e38-d8ce-4d2e-915f-c0892af829f3-ni4tle.png",
         price: 1299.00,
         description: "Frigobar Retrô Vermelho Rubi 95 Litros Midea.",
         category: "Casa&Cozinha"
-    }, {
+    },
+    {
         _id: "19",
         name: "Aspirador de Pó Vertical",
         image: "https://utfs.io/f/5dafb0eb-d234-41d7-99e9-f26f289c4feb-3yxbwm.png",
         price: 197.91,
         description: "Aspirador de Pó Vertical e Portátil WAP HIGH SPEED BLACK 1300W 2 em 1.",
         category: "Casa&Cozinha"
-    }, {
+    },
+    {
         _id: "20",
         name: "Ar Condicionado LG",
         image: "https://utfs.io/f/d7084a6e-41f9-4a55-9cc9-575485a9b851-cfontc.png",
         price: 2908.00,
         description: "Ar Condicionado Split Hi Wall Inverter LG Voice R-32 12000 BTU/h.",
         category: "Casa&Cozinha"
-    }, {
+    },
+    {
         _id: "21",
         name: "Trilha Capim Açu",
         image: "https://utfs.io/f/70df8a4c-9252-4de1-bcb1-566322680e0c-l3lt0.png",
         price: 500.00,
         description: "Trilha no parque nacional de Fernando de Noronha.",
         category: "Passeios"
-    }, {
+    },
+    {
         _id: "22",
         name: "Trilha Costa Esmeralda",
         image: "https://utfs.io/f/7a4014b8-a79e-483d-875d-178c06ee3bbb-gko8on.png",
         price: 741.00,
         description: "Tour realizado a pé pelo lado da ilha, conhecido como mar-de-dentro.",
         category: "Passeios"
-    }, {
+    },
+    {
         _id: "23",
         name: "Mergulho batismo",
         image: "https://utfs.io/f/c885af2b-7611-42b0-bd53-221db0a55b7d-8aek0l.png",
         price: 980.00,
         description: "Mergulho no parque nacional de Fernando de Noronha.",
         category: "Passeios"
-    }, {
+    },
+    {
         _id: "24",
         name: "Entardecer privativo",
         image: "https://utfs.io/f/beefb1ae-408f-401a-a4ee-f20e03c9c849-158hpp.png",
@@ -271,7 +292,7 @@ export const gift_list = [
         _id: "25",
         name: "Chaleira Elétrica",
         image: "https://utfs.io/f/e0374712-010c-44eb-af15-33b41fc24f9c-19f.png",
-        price: 180.09,
+        price: 80.00,
         description: "Chaleira Elétrica Efficient Electrolux com Capacidade de 1,8L.",
         category: "Utensílios"
     },
@@ -282,47 +303,60 @@ export const gift_list = [
         price: 403.90,
         description: "Aparelho de jantar e chá 20 peças, Oxford Flat Matcha.",
         category: "Utensílios"
-    }, {
+    },
+    {
         _id: "27",
         name: "Jogo de facas",
         image: "https://utfs.io/f/04717ee6-f49e-463f-8d9a-06d353030e33-19e.png",
         price: 228.00,
         description: "Jogo de facas 06 peças, Tramontina Polywood.",
         category: "Utensílios"
-    }, {
+    },
+    {
         _id: "28",
         name: "Panela pressão elétrica",
         image: "https://utfs.io/f/adcc1c38-06f5-47c4-be65-c8954046ac01-19c.png",
         price: 389.99,
         description: "Panela pressão elétrica Electrolux capacidade 3L.",
         category: "Utensílios"
-    }, {
+    },
+    {
         _id: "29",
         name: "Vale presente",
         image: "https://utfs.io/f/3509adc2-a35f-4a78-89c3-d827ae494f3b-nki6tq.png",
         price: 250.00,
         description: "Whey Protein e Creatina.",
         category: "Vale presente"
-    }, {
+    },
+    {
         _id: "30",
         name: "Vale presente",
         image: "https://utfs.io/f/ac0e206e-8431-48eb-82f0-c7ea0823c3f0-nki4py.png",
         price: 500.00,
         description: "Aluguel de carro para a viagem.",
         category: "Vale presente"
-    }, {
+    },
+    {
         _id: "31",
         name: "Vale presente",
         image: "https://utfs.io/f/8088c7de-79d3-4841-944b-6f16bb878e09-kck2hy.png",
         price: 1000.00,
         description: "Comprar uma Tekpix e registrar tudo na lua de mel.",
         category: "Vale presente"
-    }, {
+    },
+    {
         _id: "32",
         name: "Vale presente",
         image: "https://utfs.io/f/afaec203-f61e-49ba-9bfd-5152785faa06-kcjfif.png",
         price: 2000.00,
         description: "Taxa para a noiva não jogar o buquê para sua namorada.",
+        category: "Vale presente"
+    },
+    {
+        _id: "33",
+        name: "Vale presente",
+        image: "https://utfs.io/f/cb807276-e0c7-434f-9b9f-630c0c31225c-pvz3e1.png",
+        description: "Valor que o seu coração manda e o bolso obedece.",
         category: "Vale presente"
     }
 ]
