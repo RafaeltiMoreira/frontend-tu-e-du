8b59c82e76d7a89caf13fd43ce26ef1271daf611
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-07-11 01:19:04 -0300

    Estilização Menu fixed `HeaderShop`

--- src/components/HeaderShop/HeaderShop.jsx
+++ src/components/HeaderShop/HeaderShop.jsx
@@ -1,11 +1,46 @@
+import { useEffect } from 'react';
 import './HeaderShop.css';
 
 const HeaderShop = () => {
+  useEffect(() => {
+    const handleScrollToHeader = (e) => {
+      const targetId = e.target.getAttribute('href');
+      if (targetId === '#header') {
+        e.preventDefault();
+        const headerElement = document.querySelector(targetId);
+        const offset = 60; // Ajuste este valor conforme necessário
+        const bodyRect = document.body.getBoundingClientRect().top;
+        const elementRect = headerElement.getBoundingClientRect().top;
+        const elementPosition = elementRect - bodyRect;
+        const offsetPosition = elementPosition - offset;
+
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: 'smooth'
+        });
+      }
+    };
+
+    const menuLinks = document.querySelectorAll('a[href="#header"]');
+    menuLinks.forEach(link => {
+      link.addEventListener('click', handleScrollToHeader);
+    });
+
+    return () => {
+      menuLinks.forEach(link => {
+        link.removeEventListener('click', handleScrollToHeader);
+      });
+    };
+  }, []);
+
   return (
-    <div className='header' id='header'>
-      <div className='header-contents'>
-        <h2 className='header-h2'>Lista de nossas sugestões de presentes</h2>
-        <a href='#menu' >Lista de presentes</a>
+    <div id='header'>
+      <div className='header-menu'></div>
+      <div className='header'>
+        <div className='header-contents'>
+          <h2 className='header-h2'>Lista de nossas sugestões de presentes</h2>
+          <a href='#menu' >Lista de presentes</a>
+        </div>
       </div>
     </div>
   )
